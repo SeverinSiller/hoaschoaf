0017efb048a29b2037bee851402f49d563e05f71
Lukas <lukass.stockinger@gmail.com>
2025-09-03 14:54:37 +0200

    "Header und Herosection fix - consisten widths with rest of page"

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,31 @@
-export default {
-  base: '/', // Root-Domain passt so
+// vite.config.js
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
+import autoprefixer from 'autoprefixer';
+
+export default defineConfig({
+  base: '/',
   build: { outDir: 'docs' },
-};
+
+  server: { host: true, port: 5173, strictPort: true },
+  preview: { host: true, port: 4173, strictPort: true },
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)), // @ => /src
+    },
+  },
+
+  css: {
+    // PostCSS (Autoprefixer)
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+    // SCSS: Variablen überall automatisch injizieren
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@use "@/scss/variables.scss" as *;',
+      },
+    },
+  },
+});
